f946f9724e72e132baece3f6de58db7a174f3a19
dojcorvin <dojcorvin@gmail.com>
2026-03-28 21:20:24 +0100

    Add files via upload

---
+++ api/auth.js
@@ -0,0 +1,34 @@
+export default async function handler(req, res) {
+    const code = req.query.code;
+
+    if (!code) {
+        return res.redirect(
+            "https://discord.com/api/oauth2/authorize?client_id=TON_CLIENT_ID&redirect_uri=TON_URL_VERCEL/api/auth&response_type=code&scope=identify"
+        );
+    }
+
+    const params = new URLSearchParams();
+    params.append("client_id", "TON_CLIENT_ID");
+    params.append("client_secret", "TON_CLIENT_SECRET");
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "https://inside-rp-examen-barreau.vercel.app/api/auth");
+
+    const tokenRes = await fetch("https://discord.com/api/oauth2/token", {
+        method: "POST",
+        body: params,
+        headers: { "Content-Type": "application/x-www-form-urlencoded" }
+    });
+
+    const tokenData = await tokenRes.json();
+
+    const userRes = await fetch("https://discord.com/api/users/@me", {
+        headers: {
+            Authorization: `Bearer ${tokenData.access_token}`
+        }
+    });
+
+    const user = await userRes.json();
+
+    return res.redirect(`/frontend/index.html?user=${encodeURIComponent(JSON.stringify(user))}`);
+}
